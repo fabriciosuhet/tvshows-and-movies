b46ca26cb5d97a767633c164a9a4d7744c8f18a3
Fabricio <fabriciossuhet@gmail.com>
2023-12-03 22:34:02 -0300

    adicionando pesquisa por titulo e por genero

--- SeriesEFilmes/Controllers/MoviesController.cs
+++ SeriesEFilmes/Controllers/MoviesController.cs
@@ -20,12 +20,41 @@ namespace SeriesEFilmes.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-              return _context.Movie != null ? 
-                          View(await _context.Movie.ToListAsync()) :
-                          Problem("Entity set 'SeriesEFilmesContext.Movie'  is null.");
+            if (_context.Movie == null)
+            {
+                return Problem("O conjunto de entidades 'MvcMovieContext.Movie' é nulo.");
+            }
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
+
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
--- SeriesEFilmes/Models/Movie.cs
+++ SeriesEFilmes/Models/Movie.cs
@@ -1,15 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SeriesEFilmes.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Display(Name = "Título")]
         public string? Title { get; set; }
 
+        [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Gênero")]
         public string? Genre { get; set; }
+
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
